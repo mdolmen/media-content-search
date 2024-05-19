8ad578e9c64e265277b047eb088af6804eb07754
Mathieu Dolmen <mathieu.dolmen@gmail.com>
2024-05-19 15:12:44 +0200

    Upload file to personal cloud storage before calling Replicate.run
    Requires `rclone` to be installed.

--- backend/src/remote.py
+++ backend/src/remote.py
@@ -1,5 +1,18 @@
 import os
 import replicate
+import subprocess
+
+def get_gdrive_dl_link(link):
+    """
+    By default the gdrive link will lead to a preview of the file, the browser
+    will try to open it and then propose to download the file if it fails).
+    This function extract the file ID to build an alternative link that will
+    directly trigger a download (no preview).
+    """
+    file_id = link[link.rfind('=')+1:]
+    url_template = "https://drive.google.com/uc?export=download&id="
+
+    return url_template + file_id
 
 def transcribe(filename):
     """
@@ -12,18 +25,24 @@ def transcribe(filename):
     # TODO: get api from config.yaml (passed as arg)
     api = ""
     os.environ["REPLICATE_API_TOKEN"] = api
+    remote_storage = "remote:mcs"
 
     result = []
 
-    # When giving a local file, the lib will handle the upload. Use that for
-    # now. We'll add the possibility to upload to a cloud storage (e.g. gdrive)
-    # if necessary.
-    audio_file = open(filename, "rb")
+    # Upload file to personal cloud storage
+    subprocess.run(["rclone", "copy", filename, remote_storage])
+
+    # Get public link for that file
+    output = subprocess.run(
+        ["rclone", "link",
+         f"{remote_storage}/{filename[filename.rfind('/')+1:]}"],
+        capture_output=True)
+    public_link = get_gdrive_dl_link(output.stdout.rstrip().decode("utf-8"))
 
     output = replicate.run(
         "openai/whisper:4d50797290df275329f202e48c76360b3f22b08d28c196cbc54600319435f8d2",
         input={
-            "audio": audio_file,
+            "audio": public_link,
             "model": "large-v3",
             "language": "en",
             "translate": False,
